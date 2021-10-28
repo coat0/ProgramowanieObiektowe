1e86c2ce3d4c1d86694251f8528fdb8433768a54
coat0 <77875651+coat0@users.noreply.github.com>
2021-10-28 17:32:17 +0200

    Add files via upload

---
+++ cwiczenia1/cw2_3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace cwiczenia1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Masa, Wzrost, BMI;
+            Console.WriteLine("Podaj swoj wzrost w metrach:");
+            Wzrost = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj swoja wage w kilogramach:");
+            Masa = double.Parse(Console.ReadLine());
+            BMI = Masa / (Wzrost * Wzrost);
+            Console.WriteLine("Twoje BMI wynosi:" + BMI);
+            Console.ReadKey();
+        }
+    }
+}
