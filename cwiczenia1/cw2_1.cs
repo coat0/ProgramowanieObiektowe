67e06a2f064fb87368e3f499239a7d3a5837e7ea
coat0 <77875651+coat0@users.noreply.github.com>
2021-10-28 17:22:54 +0200

    Add files via upload

---
+++ cwiczenia1/cw2_1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace cwiczenia1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double tempc, tempf;
+            Console.WriteLine("Podaj temperature w stopniach Celsjusza:");
+            tempc = double.Parse(Console.ReadLine());
+            tempf = tempc * 9 / 5 + 32;
+            Console.WriteLine("Temperatura w C: " + tempc + " Temperatura w F: " + tempf);
+            Console.ReadKey();
+        }
+    }
+}
