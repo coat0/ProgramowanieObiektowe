670aa728aa6ad1dcac95e3f92a64a9bfe800483f
coat0 <77875651+coat0@users.noreply.github.com>
2021-10-28 17:28:11 +0200

    Add files via upload

---
+++ cwiczenia1/cw2_2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace cwiczenia1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A, B, C, Delta;
+            Console.WriteLine("Podaj wspolczynnik A:");
+            A = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj wspolczynnik B:");
+            B = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj wspolczynnik C:");
+            C = double.Parse(Console.ReadLine());
+            Delta = B * B - 4 * A * C;
+            Console.WriteLine("Delta tego rownania wynosi:" + Delta);
+            Console.ReadKey();
+        }
+    }
+}
