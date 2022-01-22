4ae00892c693861cc1692d1fc9572a72675f4f7b
coat0 <77875651+coat0@users.noreply.github.com>
2022-01-22 12:59:27 +0100

    Add files via upload

---
+++ 211128/211128_car/Car.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _211128
+{
+    class Car
+    {
+        private string marka;
+        private int rok;
+        private string model;
+        private int iloscDrzwi;
+        private double pojemnoscSilnika;
+        public double srednieSpalanie;
+
+        public Car(string x, int y)
+        {
+            if (y > 1769 && y < 2017)
+            {
+                marka = x;
+                rok = y;
+            }
+            else
+            {
+                Console.WriteLine("Podałeś zły rok");
+            }
+        }
+
+        public void ShowCar()
+        {
+            Console.WriteLine("Marka: {0, -10} Rok produkcji: {1}", marka, rok);
+        }
+
+        private double ObliczSpalanie(double road)
+        {
+            double spalanie = (srednieSpalanie * road);
+            return spalanie;
+        }
+
+        public double ObliczKosztPrzejazdu(double road, double price)
+        {
+            double koszt = ObliczSpalanie(road) * price;
+            return koszt;
+        }
+    }
+}
